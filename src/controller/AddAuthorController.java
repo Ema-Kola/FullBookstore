3daf4294778cfa1b24e26f1d7c1be8a781d09216
Ema Kola <ekola22@epoka.edu.al>
2024-12-28 05:19:00 +0100

    small refactor plus some unit tests

--- src/controller/AddAuthorController.java
+++ src/controller/AddAuthorController.java
@@ -11,18 +11,33 @@ public class AddAuthorController {
 
         private AuthorsDAO authorsDAO;
         private final AddAuthorView addAuthorView;
+        private final Alert alert1;
+        private final Alert alert2;
+        private final Alert alert3;
 
 
-        public AddAuthorController(AddBookView prevView){
+
+
+    public AddAuthorController(AddBookView prevView){
             this.addAuthorView = new AddAuthorView();
             addAuthorView.getSubmitButton().disableProperty().bind(addAuthorView.getAuthorNameTf().textProperty().isEmpty().or(addAuthorView.getAuthorSurnameTf().textProperty().isEmpty()).or(addAuthorView.getGendercomboBox().valueProperty().isNull()));
 
+            alert1 = new Alert(Alert.AlertType.CONFIRMATION);
+            alert1.setContentText("Author saved successfully!");
+
+            alert2 = new Alert(Alert.AlertType.ERROR);
+            alert2.setContentText("Failed to save author.");
+
+            alert3 = new Alert(Alert.AlertType.ERROR);
+            alert3.setContentText("This author already exists. ");
+            authorsDAO=new AuthorsDAO();
             addAuthorView.getSubmitButton().setOnAction(e -> {
+                System.out.println("Submit button clicked!");
                 String name = addAuthorView.getAuthorName();
                 String lastname = addAuthorView.getAuthorLastName();
                 Gender gender= addAuthorView.getGender();
                 Author newAuthor = new Author(name, lastname, gender);
-                authorsDAO=new AuthorsDAO();
+
                 if(authorsDAO.searchAuthor(name, lastname)==null) {
 
                     if (authorsDAO.create(newAuthor)) {
@@ -30,21 +45,14 @@ public class AddAuthorController {
                         this.addAuthorView.getAuthorNameTf().clear();
                         this.addAuthorView.getAuthorSurnameTf().clear();
                         System.out.println("Author saved successfully");
-                        Alert a = new Alert(Alert.AlertType.CONFIRMATION);
-                        a.setContentText("Author saved succesfully!");
-                        a.showAndWait();
+                        alert1.showAndWait();
                     } else{
-                        System.out.println("Operation failed");
-                        Alert a = new Alert(Alert.AlertType.ERROR);
-                        a.setContentText("Failed to save author.");
-                        a.showAndWait();
+                        alert2.showAndWait();
                     }
 
                 }else{
+                    alert3.showAndWait();
                     System.out.println("This author already exists.");
-                    Alert a = new Alert(Alert.AlertType.ERROR);
-                    a.setContentText("This author already exists. ");
-                    a.showAndWait();
                 }
 
 
@@ -54,6 +62,22 @@ public class AddAuthorController {
 
         public AddAuthorView getView(){
             return this.addAuthorView;
+        }
+
+
+        public void setAuthorsDAO(AuthorsDAO authorsDAO) {
+            this.authorsDAO = authorsDAO;
+        }
+        public Alert getAlert1() {
+            return alert1;
+        }
+
+        public Alert getAlert2() {
+            return alert2;
+        }
+
+        public Alert getAlert3() {
+            return alert3;
         }
 
 
